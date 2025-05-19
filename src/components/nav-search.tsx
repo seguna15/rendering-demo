00bb2e7516a536ea2021f607d22af4d71af3f60e
seguna15 <seguna15@gmail.com>
2025-05-19 16:47:36 +0100

    use client pushed down the tree

--- src/components/nav-search.tsx
+++ src/components/nav-search.tsx
@@ -1,7 +1,11 @@
 "use client"
 
+import { useState } from "react";
+
 export const NavSearch = () => {
   console.log(`NavSearch rendered`);
+  const [search, setSearch] = useState("");
+
 
   return <div>Nav Search Input</div>;
 };
